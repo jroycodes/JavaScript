6c53da7d57db89751ea1241a5fb5e36cb1882e8d
Jonathan Roy <jonaisroy@gmail.com>
2024-06-04 12:20:40 +0300

    two sum

--- script.js
+++ script.js
@@ -185,3 +185,16 @@ function getAge(person) {
   } else return person.yearOfDeath - person.yearOfBirth;
 }
 console.log(findTheOldest(people));
+
+// Two Sum
+function twoSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        let output = [i, j];
+        return output;
+      }
+    }
+  }
+}
+console.log(twoSum([1, 2, 3], 4));
